602518253083de31cbf0017a80c44f6e348787f1
Marcos Antônio de Souza Silva <marcos@ssig.tech>
2024-05-18 22:33:47 -0300

    Atualiza arquivo de configuração WSGI

--- integra/wsgi.py
+++ integra/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'integra.settings')
+if os.environ.get('DJANGO_SETTINGS_MODULE') is None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'integra.settings.production')
 
 application = get_wsgi_application()
