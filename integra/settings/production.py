1d5c007f726e84cfff9bcbeb25a1c52a3a2869e3
Marcos Antônio de Souza Silva <marcos@ssig.tech>
2024-05-18 22:15:12 -0300

    Atualiza configuração para receber informações do banco de dados

--- integra/settings/production.py
+++ integra/settings/production.py
@@ -13,3 +13,14 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+
+DATABASES = {
+    "default": {
+        "ENGINE":  os.environ.get('DATABASE_ENGINE', 'django.db.backends.postgresql'),
+        "NAME": os.environ.get('DATABASE_NAME'),
+        "USER": os.environ.get('DATABASE_USER'),
+        "PASSWORD": os.environ.get('DATABASE_PASSWORD'),
+        "HOST": os.environ.get('DATABASE_HOST'),
+        "PORT": os.environ.get('DATABASE_PORT', '5432'),
+    }
+}
